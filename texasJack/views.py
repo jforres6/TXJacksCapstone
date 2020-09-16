35dc64c13b81b4429d2ddec5056d8ac69b96d375
ry-cho <ryancho213@gmail.com>
2020-09-15 21:54:50 -0400

    Revert "Add files via upload"
    This reverts commit f3b438aae8e86d0da34d7c1df665947f49a9b015.

--- texasJack/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render 
-
-def homepage(request): 
-    # return HttpResponse('homepage')
-    return render(request, 'homepage.html')
-
