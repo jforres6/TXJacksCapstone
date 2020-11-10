0420c221407805a860d17d0ddfe9f282f4e0ae9d
jforres6 <74158989+jforres6@users.noreply.github.com>
2020-11-09 22:14:05 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -20,3 +20,13 @@ app.listen(3000, function() {
     console.log("Server started on port 3000");
 })
 
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://TJUser1:TexasJackMongoDB@cluster0.b04vt.mongodb.net/TexasJacks?retryWrites=true&w=majority", {useNewUrlParser: true});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connectionasdf error:'));
+ 
+db.once('open', function() {
+  console.log("Successfully connected to MongoDB!");
+});
