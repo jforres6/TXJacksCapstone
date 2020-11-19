87fe0911e6c389d6b1f9c6e9400e9883edf31354
Luis Moran <louiemoran@Luiss-iMac.local>
2020-11-19 18:22:38 -0500

    fixed issue with merge

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 
-=======
->>>>>>> master
 
 const app = express(); 
 
